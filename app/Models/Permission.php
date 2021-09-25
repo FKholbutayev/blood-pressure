a01dd5977122a7c7e9cd5aeeae0adedcf0e8d2b5
FKholbutayev <ht.uzpharma@gmail.com>
2021-09-25 15:26:45 +0200

    add models

---
+++ app/Models/Permission.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    const DOCTOR = 'Doctor';
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function scopeDoctor($query)
+    {
+        return $query->where('title', self::DOCTOR);
+    }
+}
