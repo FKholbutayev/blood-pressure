15f517e929337a43597f8a822cbfaf3d08802acf
FKholbutayev <ht.uzpharma@gmail.com>
2021-09-25 15:27:18 +0200

    add migrations

---
+++ database/migrations/2021_09_22_141330_create_blood_pressures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBloodPressuresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blood_pressures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->references('id')->on('patients')->cascadeOnDelete();
+            $table->unsignedInteger('systolic_pressure');
+            $table->unsignedInteger('diastolic_pressure');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blood_pressures');
+    }
+}
